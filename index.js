b62bdcd9938bd4edd0a1a12052a32a1dae8bf0b9
Addons007 <b8h7wfvsdx@privaterelay.appleid.com>
2025-11-21 15:31:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,35 @@ const manifest = {
   version: "1.0.0",
   name: "IPTV M3U Addon",
   description: "Stream addon for custom IPTV M3U playlists",
-  types: ["movie", "series"],
-  resources: ["stream"],
-  catalogs: [],
-  behaviorHints: {
-    configurationRequired: false
-  }
+  types: ["tv"],
+  resources: ["catalog", "stream"],
+  catalogs: [
+    { type: "tv", id: "iptv", name: "IPTV Channels" }
+  ],
+  behaviorHints: { configurationRequired: false }
 };
 
 const builder = new addonBuilder(manifest);
 
+builder.defineCatalogHandler(() => ({
+  metas: [
+    { id: "channel1", type: "tv", name: "Sample Channel 1" },
+    { id: "channel2", type: "tv", name: "Sample Channel 2" }
+  ]
+}));
+
 builder.defineStreamHandler(({ id }) => {
-  return Promise.resolve({
-    streams: [
-      {
-        title: "Test Stream",
-        url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-      }
-    ]
-  });
+  if (id === "channel1") {
+    return Promise.resolve({
+      streams: [{ title: "Channel 1 Stream", url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8" }]
+    });
+  }
+  if (id === "channel2") {
+    return Promise.resolve({
+      streams: [{ title: "Channel 2 Stream", url: "https://test-streams.mux.dev/test_001/stream.m3u8" }]
+    });
+  }
+  return Promise.resolve({ streams: [] });
 });
 
 const addonInterface = builder.getInterface();
@@ -34,9 +44,16 @@ module.exports = async (req, res) => {
   if (pathname === "/manifest.json") {
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(addonInterface.manifest));
-  } else if (pathname === "/stream.json") {
-    const { id } = req.query;
-    const result = await addonInterface.get("stream", { id });
+  } else if (pathname === "/catalog/tv/iptv.json") {
+    const result = await addonInterface.get("catalog", { type: "tv", id: "iptv" });
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result));
+  } else if (pathname === "/stream/channel1.json") {
+    const result = await addonInterface.get("stream", { id: "channel1" });
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(result));
+  } else if (pathname === "/stream/channel2.json") {
+    const result = await addonInterface.get("stream", { id: "channel2" });
     res.setHeader("Content-Type", "application/json");
     res.end(JSON.stringify(result));
   } else {
